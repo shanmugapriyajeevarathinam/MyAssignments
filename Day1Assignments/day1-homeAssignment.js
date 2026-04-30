ba43f945ebb8c0b4f151b2220ab7784bbc7d1fb9
shanmugapriyajeevarathinam <jeevashanmu2016@gmail.com>
2026-04-30 23:58:48 +0530

    Add files via upload
    Week1/Day1Assignments

---
+++ Day1Assignments/day1-homeAssignment.js
@@ -0,0 +1,17 @@
+let firstName = "Shanmugapriya";
+let companyName = "TCS";
+let mobileNumber = 123456789;
+let isAutomation = true;
+let hasPlaywright
+
+console.log("FirstName is" , firstName ,"And", "Data Typeof FirstName is " , typeof(firstName));
+
+console.log("CompanyName is" , companyName ,"And", "Data Typeof CompanyName is " , typeof(companyName));
+
+console.log("MobileNumber is" , mobileNumber ,"And", "Data Typeof MobileNumber is " , typeof(mobileNumber));
+
+console.log("isAutomation", isAutomation ,"And", "Data Typeof isAutomation is " , typeof(isAutomation));
+
+console.log("hasPlaywright", hasPlaywright ,"And", "Data Typeof hasPlaywright is " , typeof(hasPlaywright));
+
+
